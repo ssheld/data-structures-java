815ec832497776068766ecf19a620fbbfaab590f
ssheld <20730278+ssheld@users.noreply.github.com>
2022-06-02 11:17:11 -0400

    Create linked list based stack

---
+++ myliststack/src/java/com/ssheld/Application.java
@@ -0,0 +1,8 @@
+package com.ssheld;
+
+public class Application {
+    
+    public static void main(String[] args) {
+        System.out.println("Linked List implementation of stack data structure");
+    }
+}
---
+++ myliststack/test/java/MyListStackTest.java
@@ -0,0 +1,45 @@
+import com.ssheld.MyListStack;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class MyListStackTest {
+
+
+    private MyListStack<Integer> arr;
+
+    @BeforeEach
+    void setup() {
+        arr = new MyListStack<>();
+    }
+
+    @Test
+    void getTopOfStack() {
+        arr.push(3);
+        arr.push(5);
+        arr.push(2);
+
+
+        assertEquals(2, arr.pop());
+        assertEquals(5, arr.pop());
+        assertEquals(1, arr.getSize());
+    }
+
+    @Test
+    void getSizeAccuratelyKeepsCurrentSizeOfArrayAsItemsAreAdded() {
+        arr.push(2);
+        arr.push(4);
+
+        assertEquals(2, arr.getSize());
+    }
+
+    @Test
+    void isEmptySuccessfullyChecksIfArrayIsEmptyAfterAddingItems() {
+        arr.push(4);
+        arr.push(5);
+
+        assertFalse(arr.isEmpty());
+    }
+}
